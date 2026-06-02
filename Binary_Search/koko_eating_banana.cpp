105cddb12ded0dc884335f838937f155a6a20dce
parasseth2006 <parasseth2006@gmail.com>
2026-06-02 23:53:47 +0530

    Koko eating bananas

---
+++ Binary_Search/koko_eating_banana.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    long long calculateTotalHours(vector<int>&piles, int hourly){
+        int n = piles.size();
+        long long totalhours = 0;
+        for(int i = 0; i < n; i++){
+            totalhours+= ceil((double)piles[i]/(double)hourly);
+        }
+        return totalhours;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int max = *max_element(piles.begin(), piles.end());
+        int low = 1, high = max;
+        while( low<=high){
+            int mid = (low+high)/2;
+            long long totalhours = calculateTotalHours(piles, mid);
+            if(totalhours<= h){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
