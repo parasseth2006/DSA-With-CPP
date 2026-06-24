8aad37691262be195fe91c3e7fc7b8e8dd750c70
parasseth2006 <parasseth2006@gmail.com>
2026-06-24 23:55:15 +0530

    Search in LL

---
+++ Linked_List/Search_in_LL.cpp
@@ -0,0 +1,69 @@
+//Program to insert a new node at the beginning (head) of the Linked List
+#include <iostream>
+using namespace std;
+// Node class to represent each node in the linked list
+class Node{
+    public:
+    int data;
+    Node* next;
+    // Constructor with only data
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+//Class for list
+class List{
+    Node* head;
+    Node* tail;
+    public:
+    List(){
+        head = tail = NULL;
+    }
+    //Function to insert at the end of LL
+    void pushBack(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    //Function to print a LL
+    void printLL(){
+        Node* temp = head;
+        while(temp!=NULL){
+            cout<< temp->data<<" -> ";
+            temp = temp->next;
+        }
+        cout<<"NULL" <<endl;
+    }
+    //Function to search in LL
+    int Search(int key){
+        Node* temp = head;
+        int index = 0;
+        while(temp!=NULL){
+            if(temp -> data == key){
+                return index;
+            }
+            temp = temp -> next;
+            index++;
+        }
+        return -1;
+    }
+};
+
+//Main function
+int main(){
+    List ll;
+    ll.pushBack(1);
+    ll.pushBack(2);
+    ll.pushBack(3);
+    ll.pushBack(4);
+    ll.printLL();
+    cout<< ll.Search(2);
+    return 0;
+
+}
