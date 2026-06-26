17156ef168d62b49580adaa76184b9527a073a76
parasseth2006 <parasseth2006@gmail.com>
2026-06-26 22:10:41 +0530

    Problems on LL

--- Linked_List/Problems_On_Singly_LL/Reverse_LL.cpp
+++ Linked_List/Problems_On_Singly_LL/Reverse_LL.cpp
@@ -20,17 +20,29 @@ class ListNode{
 
 };
 
-//Function to Find middle of LL
-ListNode* middleNode(ListNode* head) {
-    ListNode* slow = head;
-    ListNode* fast = head;
-    while(fast != NULL && fast ->next != NULL){
-        slow = slow -> next;
-        fast = fast -> next -> next;
+//Function to reverse LL
+ListNode* reverseList(ListNode* head) {
+    ListNode* temp = head;
+    ListNode* prev = NULL;
+    while (temp != NULL){
+        ListNode* front = temp -> next;
+        temp -> next = prev;
+        prev = temp;
+        temp = front;
     }
-    return slow;
+    return prev;
 }
 
+//Print LL
+void printLL(ListNode* head){
+    ListNode* temp = head;
+    while(temp!=NULL){
+        cout<< temp->data<<"  ";
+        temp = temp->next;
+    }
+    cout<< endl;
+    
+}
 //Main function
 int main() {
     // Creating a sample linked list: 
@@ -39,10 +51,8 @@ int main() {
     head->next->next = new ListNode(3);
     head->next->next->next = new ListNode(4);
     head->next->next->next->next = new ListNode(5);
-
-    // Find the middle node
-    ListNode* middlenode = middleNode(head);
-    cout << "The middle node value is: " << middlenode->data << endl;
-
+    printLL(head);
+    head = reverseList(head);
+    printLL(head);
     return 0;
 }
