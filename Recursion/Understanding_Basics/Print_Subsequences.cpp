4ac3c49479a72eeea1b4efba3180db740712de42
parasseth2006 <parasseth2006@gmail.com>
2026-07-04 22:06:20 +0530

    Print subsequences

---
+++ Recursion/Understanding_Basics/Print_Subsequences.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void PrintF(int ind, vector<int>ds, int arr[], int n){
+    if(ind == n){
+        for(auto it : ds){
+            cout<<it;
+        }
+        cout << endl;
+        return;
+    }
+    //Take or put the particular index in subsequence
+    ds.push_back(arr[ind]);
+    PrintF(ind+1, ds, arr, n);
+    ds.pop_back();
+
+    //Not take this element is not added to subsequence
+    PrintF(ind+1, ds, arr, n);
+}
+
+int main(){
+    int arr[] = {3,1,2};
+    int  n = 3;
+    vector<int>ds;
+    PrintF(0,ds,arr,n);
+    return 0;
+
+}
